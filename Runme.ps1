13d8eb391259959fb5c63232e665cec433f51c12
dixonwi3 <44004099+dixonwi3@users.noreply.github.com>
2019-04-26 19:23:06 -0400

    Delete Runme.ps1

--- Runme.ps1
+++
@@ -1,5 +0,0 @@
-﻿# may have to type Set-ExecutionPolicy Unrestricted in windows shell, then select option "A" when prompted
-#
-docker pull openshift/base-centos7
-docker run openshift/base-centos7
-pause
